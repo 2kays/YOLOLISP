c2a87c179b883da9db61d1a7a56b03801f3fc949
2kays <2kays@users.noreply.github.com>
2021-08-28 16:07:25 +0100

    Compiler cleanup, restructuring, and a macro system
    * Added macro system (`yl-macroexpand`)
    * Add `do` primitive for sequencing
    * Assignment primitive renamed from `set` to `assign`
    * `set` and `when` refactored as macros
    * `rinc/rdec`->`inc/dec`, `inc/dec`->`preinc/predec`

--- yololisp-compiler.el
+++ yololisp-compiler.el
@@ -6,8 +6,8 @@
     (abs . "abs ")
     (not . "not ")
     (! . "!")
-    (inc . "++")
-    (dec . "--")
+    (preinc . "++")
+    (predec . "--")
     ;; trig
     (sin . "sin ")
     (cos . "cos ")
@@ -18,13 +18,14 @@
   "one-arg operators on left-side of value")
 
 (defvar yl-unary-right
-  '((rinc . "++")
-    (rdec . "--")
+  '((inc . "++")
+    (dec . "--")
     (fact . "!"))
   "one-arg operators on right-side of value")
 
 (cl-defun yl-compile-expr (expr &optional (level 0))
-  "Compiles a YOLOLISP expression `expr'."
+  "Compiles a YOLOLISP expression `EXPR'.
+Optional `LEVEL' refers to nesting level, determining parenthesis use in nested expressions."
   (cl-etypecase expr
     (cons (let ((result (let* ((op (car expr))
                                (arg1 (cadr expr))
@@ -55,24 +56,27 @@
     (integer (number-to-string expr))
     (float   (number-to-string expr))))
 
-(defun yl-compile-set (var expr)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; YOLOL PRIMITIVE COMPILER
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun yl-compile-assign (var expr)
   (concat (symbol-name var) " = " (yl-compile-expr expr)))
 
-(defun yl-compile-when (condition tbranch)
-  (concat "if "
-          (yl-compile-expr condition)
-          " then "
-          (yl-compile tbranch)
-          " end"))
-
-(defun yl-compile-if (condition tbranch fbranch)
+(defun yl-compile-if (condition tbranch &optional fbranch)
+  (if fbranch
+      (concat "if "
+              (yl-compile-expr condition)
+              " then "
+              (yl-compile-form tbranch)
+              " else "
+              (yl-compile-form fbranch)
+              " end")
     (concat "if "
-          (yl-compile-expr condition)
-          " then "
-          (yl-compile tbranch)
-          " else "
-          (yl-compile fbranch)
-          " end"))
+            (yl-compile-expr condition)
+            " then "
+            (yl-compile-form tbranch)
+            " end")))
 
 (defun yl-compile-goto (expr)
   (concat "goto " (yl-compile-expr expr)))
@@ -80,21 +84,52 @@
 (defun yl-compile-comment (comment)
   (concat "// " comment))
 
-(defun yl-compile-form (form &optional env)
-  (let ((sym (car form)))
-    (cl-ecase sym
-      (set  (yl-compile-set     (cadr form) (caddr form)))
-      (when (yl-compile-when    (cadr form) (caddr form)))
-      (if   (yl-compile-if      (cadr form) (caddr form) (cadddr form)))
-      (goto (yl-compile-goto    (cadr form)))
-      (//   (yl-compile-comment (cadr form))))))
-
-(defun yl-compile-forms (forms)
+(defun yl-compile-do (forms)
   (mapconcat #'identity (mapcar #'yl-compile-form forms) "\n"))
 
-(defmacro yl (&rest forms)
-  `(yl-compile-forms ',forms))
+(defun yl-compile-form (form &optional env)
+  (let ((sym (car form)))
+    (if (yl-get-macro sym)
+        (yl-compile-form (yl-macroexpand form))
+      (cl-ecase sym
+        (do     (yl-compile-do (cdr form)))
+        (assign (yl-compile-assign  (cadr form) (caddr form)))
+        (if   (yl-compile-if      (cadr form) (caddr form) (cadddr form)))
+        (goto (yl-compile-goto    (cadr form)))
+        (//   (yl-compile-comment (cadr form)))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; YOLOLISP-MACROS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun yl-macro-when (form)
+  `(if ,(cadr form) ,(caddr form)))
+
+(defun yl-macro-unless (form)
+  `(if (! ,(cadr form)) ,(caddr form)))
+
+(defun yl-macro-set (form)
+  (let* ((pairs (-partition-all 2 (cdr form)))
+         (assign-pairs (mapcar (lambda (pair) (cons 'assign pair)) pairs)))
+    `(do ,@assign-pairs)))
+
+(defvar yl-macro-registry
+  '((when   . yl-macro-when)
+    (unless . yl-macro-unless)
+    (set    . yl-macro-set)))
+
+(defun yl-get-macro (symbol)
+  (assoc symbol yl-macro-registry))
+
+(defun yl-macroexpand (form)
+  (funcall (cdr (yl-get-macro (car form))) form))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CONVENIENCE MACROS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmacro yl (&rest forms)
+  `(yl-compile-do ',forms))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; TESTING
@@ -109,8 +144,16 @@
   (yl--test (yl-compile-expr '(== :a :b)) ":a == :b")
   t)
 
-;; statement compiler tests
+;; primitive statement compiler tests
 (progn
-  (yl--test (yl-compile-form '(when (= a 10) (set b 4))) "if a = 10 then b = 4 end")
-  (yl--test (yl-compile-forms '((set :a 10) (// "Poooop!"))) ":a = 10\n// Poooop!")
+  (yl--test (yl-compile-form '(do (assign a 1) (assign b 2))) "a = 1
+b = 2")
+  (yl--test (yl-compile-form '(do (if (= a 10) (assign b 4)))) "if a = 10 then b = 4 end")
+  (yl--test (yl-compile-form '(do (assign :a 10) (// "Comment!"))) ":a = 10\n// Comment!")
+  t)
+
+;; macro expansion tests
+(progn
+  (yl--test (yl-compile-form '(set a 1 b 2)) "a = 1
+b = 2")
   t)
